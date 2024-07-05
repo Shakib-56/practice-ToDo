ba911ddbc63aac216fa85cf5ebf7bf5736076073
Shakib Hossen <hossenmdshakib47@gmail.com>
2024-07-06 03:32:13 +0600

    add section and other functionality

--- script.js
+++ script.js
@@ -1,57 +1,67 @@
 let inputBox = document.querySelector("#inputBox");
 let list = document.querySelector("#list");
+const modal = document.querySelector(".modal");
 
-// window.onload = getLocalStorage();
-document.addEventListener("DOMContentLoaded", (e) => {
-  // e.preventDefault();
-  console.log("the Dom fully loaded");
-  getLocalStorage();
+// // window.onload = getLocalStorage();
+// document.addEventListener("DOMContentLoaded", (e) => {
+//   // e.preventDefault();
+//   console.log("the Dom fully loaded");
+//   getLocalStorage();
+//   setLocalStorage();
+// });
+
+// let inputData = [];
+
+const addItem = () => {
+  if (inputBox.value === "") {
+    alert("You must write something");
+  } else {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = inputBox.value;
+    list.appendChild(listItem);
+    let i = document.createElement("i");
+    listItem.appendChild(i);
+  }
+  inputBox.value = "";
   setLocalStorage();
-});
+};
 
-let inputData = [];
+list.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("done");
+      setLocalStorage();
+    } else if (e.target.tagName === "I") {
+      e.target.parentElement.remove();
+      setLocalStorage();
+    }
+  },
+  false
+);
+
+const setLocalStorage = () => {
+  localStorage.setItem("ListItem", list.innerHTML);
+};
+
+const showTask = () => {
+  list.innerHTML = localStorage.getItem("ListItem");
+};
+showTask();
+
+// const reset = () => {
+//   localStorage.removeItem("ListItem");
+//   location.reload();
+// };
+
+const isValidInput = (value) => {
+  return value !== null && value.trim() !== " ";
+};
 
 inputBox.addEventListener("keyup", function (e) {
   if (e.key === "Enter" && isValidInput(this.value)) {
     addItem(this.value);
     this.value = "";
+    setLocalStorage();
   }
 });
-
-const addItem = (inputBox) => {
-  let listItem = document.createElement("li");
-  listItem.innerHTML = `${inputBox} <i></i>`;
-  listItem.addEventListener("click", function () {
-    this.classList.toggle("done");
-  });
-
-  listItem.querySelector("i").addEventListener("click", function (e) {
-    e.stopPropagation();
-    listItem.remove();
-  });
-  list.appendChild(listItem);
-
-  inputData.push(inputBox);
-  console.log(inputData);
-  setLocalStorage();
-};
-
-const setLocalStorage = () => {
-  const data = localStorage.setItem("ListItem", JSON.stringify(inputData));
-};
-
-const getLocalStorage = () => {
-  const data = JSON.parse(localStorage.getItem("ListItem"));
-  if (!data) return;
-  inputData = data;
-  inputData.forEach((item) => addItem(item, false));
-};
-
-const reset = () => {
-  localStorage.removeItem("ListItem");
-  location.reload();
-};
-
-const isValidInput = (value) => {
-  return value !== null && value.trim() !== " ";
-};
